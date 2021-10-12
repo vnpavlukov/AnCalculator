ca7fbcb5caf305920449a70f544ffd996e2f85d8
Vladimir <alfa.voland@gmail.com>
2021-10-12 09:23:44 +0300

    idea settings and created userInputFun

--- app/src/main/java/com/example/ancalculator/MainActivity.kt
+++ app/src/main/java/com/example/ancalculator/MainActivity.kt
@@ -1,11 +1,19 @@
-package com.example.ancalculator
+package com.example.petsecond
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun userInputFun(str: String) {
+        userInput
+    }
+
 }
